7c60ea4c6567582b22024838476ffe45d2200b5f
Ali Hamdi <82575348+AliHamdi63@users.noreply.github.com>
2024-12-08 01:43:43 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,23 +5,20 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
-      </Head>
+    html lang="en">
+    <head>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
 
-      {/* <AuthProvider> */}
-
+    </head>
+    <body>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
 
-      {/* </AuthProvider> */}
-
-
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
 
-    </>
+    </body>
+  </html>
   )
 }
 
